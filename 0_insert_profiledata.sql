8d8d74035de70cafe585693f56d020f37201bd7b
Takuto Kotsubo <aseries2765@gmail.com>
2018-09-27 20:00:28 +0900

    first commit

---
+++ 0_insert_profiledata.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS original.profiledata;
+CREATE TABLE original.profiledata(
+  house_num char(10),
+  tmp char(10), -- 不要列削除する
+  qu_genre_code CHAR(10),
+  question_code CHAR(10),
+  answer_code CHAR(10)
+)
+-- 主キーを設定する
+DISTRIBUTED BY(house_num,qu_genre_code,question_code,answer_code);
+-- データを挿入する
+COPY 
+  original.profiledata 
+FROM 
+  '/home/takut0/data/ProfileData_2.csv' CSV HEADER; 
