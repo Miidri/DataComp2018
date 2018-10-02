d3b0c558e766a7d9fca3b035e8267265d7604c20
Takuto Kotsubo <aseries2765@gmail.com>
2018-10-02 18:18:49 +0900

    修正

--- sql/0_insert_profiledata.sql
+++ sql/0_insert_profiledata.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS original.profiledata;
 CREATE TABLE original.profiledata(
   house_num char(10),
-  tmp char(10), -- 不要列削除する
+  tmp char(10), -- 不要列, 前処理において削除する
   qu_genre_code CHAR(10),
   question_code CHAR(10),
   answer_code CHAR(10)
