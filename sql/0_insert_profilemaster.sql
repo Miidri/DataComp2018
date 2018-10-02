6550de3923378777525ea24fc3329045113fd937
Takuto Kotsubo <aseries2765@gmail.com>
2018-10-02 17:58:52 +0900

    first commit

---
+++ sql/0_insert_profilemaster.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS original.profilemaster;
+CREATE TABLE original.profilemaster(
+  qu_genre_code CHAR(100),
+  qu_genre CHAR(100),
+  question_code CHAR(100),
+  question CHAR(100),
+  question_type CHAR(100),
+  answer_code CHAR(100),
+  answer CHAR(100)
+)
+-- 主キーを設定する
+DISTRIBUTED BY(qu_genre_code,question_code, answer_code);
+-- データを挿入する
+COPY 
+  original.profilemaster
+FROM 
+  '/home/takut0/data/ProfileMaster.csv' CSV HEADER; 
