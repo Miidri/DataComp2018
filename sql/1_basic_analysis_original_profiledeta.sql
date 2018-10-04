6db5591fbbe3de0a34c94a899e573d90f296fd92
takuto0831 <aseries2765@gmail.com>
2018-10-04 13:08:57 +0900

    first commit

---
+++ sql/1_basic_analysis_original_profiledeta.sql
@@ -0,0 +1,35 @@
+-- basic analysis for raw data -- 
+SELECT
+  MAX(house_num_len),
+  MAX(ind_num_len),
+  MAX(qu_genre_code_len),
+  MAX(question_code_len),
+  MAX(answer_code_len),
+  MIN(house_num_len),
+  MIN(ind_num_len),
+  MIN(qu_genre_code_len),
+  MIN(question_code_len),
+  MIN(answer_code_len),
+  COUNT(DISTINCT(house_num)),
+  COUNT(DISTINCT(ind_num)),
+  COUNT(DISTINCT(qu_genre_code)),
+  COUNT(DISTINCT(question_code)),
+  COUNT(DISTINCT(answer_code)),
+  SUM(CASE WHEN house_num IS NULL THEN 1 ELSE 0 END),
+  SUM(CASE WHEN ind_num IS NULL THEN 1 ELSE 0 END),
+  SUM(CASE WHEN qu_genre_code IS NULL THEN 1 ELSE 0 END),
+  SUM(CASE WHEN question_code IS NULL THEN 1 ELSE 0 END),
+  SUM(CASE WHEN answer_code IS NULL THEN 1 ELSE 0 END)
+  
+FROM(
+  -- 文字列を長さに変換する仮テーブル -- 
+  SELECT
+    CHAR_LENGTH(house_num) AS house_num_len,
+    CHAR_LENGTH(ind_num) AS ind_num_len,
+    CHAR_LENGTH(qu_genre_code) AS qu_genre_code_len,
+    CHAR_LENGTH(question_code) AS question_code_len,
+    CHAR_LENGTH(answer_code) AS answer_code_len
+  FROM
+    original.profiledata
+) AS length_table;
+ 
---
+++ sql/1_basic_analysis_original_profilemaster.sql
@@ -0,0 +1,44 @@
+-- basic analysis for raw data -- 
+SELECT
+  MAX(qu_genre_code_len),
+  MAX(qu_genre_len),
+  MAX(question_code_len),
+  MAX(question_len),
+  MAX(question_type_len),
+  MAX(answer_code_len),
+  MAX(answer_len),
+  MIN(qu_genre_code_len),
+  MIN(qu_genre_len),
+  MIN(question_code_len),
+  MIN(question_len),
+  MIN(question_type_len),
+  MIN(answer_code_len),
+  COUNT(DISTINCT(answer)),
+  COUNT(DISTINCT(qu_genre_code)),
+  COUNT(DISTINCT(qu_genre)),
+  COUNT(DISTINCT(question_code)),
+  COUNT(DISTINCT(question)),
+  COUNT(DISTINCT(question_type)),
+  COUNT(DISTINCT(answer_code)),
+  COUNT(DISTINCT(answer)),
+  SUM(CASE WHEN qu_genre_code IS NULL THEN 1 ELSE 0 END),
+  SUM(CASE WHEN qu_genreIS NULL THEN 1 ELSE 0 END),
+  SUM(CASE WHEN question_code IS NULL THEN 1 ELSE 0 END),
+  SUM(CASE WHEN question IS NULL THEN 1 ELSE 0 END),
+  SUM(CASE WHEN question_type IS NULL THEN 1 ELSE 0 END),
+  SUM(CASE WHEN answer_code IS NULL THEN 1 ELSE 0 END),
+  SUM(CASE WHEN answer IS NULL THEN 1 ELSE 0 END),
+FROM(
+  -- 文字列を長さに変換する仮テーブル --
+  SELECT
+    *,
+    CHAR_LENGTH(qu_genre_code) AS qu_genre_code_len,
+    CHAR_LENGTH(qu_genre) AS qu genre_len, 
+    CHAR_LENGTH(question_code) AS question_code_len,
+    CHAR_LENGTH(question) AS question_len,
+    CHAR_LENGTH(question_type) AS question_type_len,
+    CHAR_LENGTH(answer_code) AS answer_code_len,
+    CHAR_LENGTH(answer) AS answer_len
+  FROM
+    original.profilemaster
+    ) AS length_table;
