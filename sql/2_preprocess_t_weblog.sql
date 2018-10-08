4fb53ab2b6ad9ac67b39524204a7cf8ec4391c71
takuto0831 <aseries2765@gmail.com>
2018-10-09 01:00:37 +0900

    完成

--- sql/2_preprocess_t_weblog.sql
+++ sql/2_preprocess_t_weblog.sql
@@ -23,13 +23,19 @@ DISTRIBUTED BY (web_index, web_date, web_start_time, pc_flag, unknown1, unknown2
 INSERT INTO processed.t_weblog
 SELECT
   TO_NUMBER(web_index,'99999'),
-  TO_DATE(web_date,'YYYY-MM-DD'),
-  -- 要修正 -- 
-  CASE WHEN to_number(web_start_time,'999999') >= 2500 THEN to_timestamp(br_date||'0'||TRIM(to_char(to_number(start_time,'9999') - 2400,'999')),'YYYY-MM-DDHH24MI') + interval '1day'
-	     WHEN to_number(web_start_time,'999999') >= 2410 THEN to_timestamp(br_date||'00'||TRIM(to_char(to_number(start_time,'9999') - 2400,'99')),'YYYY-MM-DDHH24MI') + interval '1day'
-	     WHEN to_number(web_start_time,'999999') >= 2400 THEN to_timestamp(br_date||'000'|| TRIM(to_char(to_number(start_time,'9999') - 2400,'9')),'YYYY-MM-DDHH24MI') + interval '1day'
-		 ELSE to_timestamp(br_date||start_time,'YYYY-MM-DDHH24MI') END AS br_start_datetime,
-  ------------
+  -- TO_DATE(web_date,'YYYY-MM-DD'), --
+  CASE 
+   WHEN to_number(web_start_time,'999999') >= 250000
+    THEN to_timestamp(web_date||'0'||TRIM(to_char(to_number(web_start_time,'999999') - 240000,'99999')),'YYYY-MM-DDHH24MISS') + INTERVAL '1day' 
+   WHEN to_number(web_start_time,'999999') >= 241000 AND to_number(web_start_time,'999999') < 250000
+    THEN to_timestamp(web_date||'00'||TRIM(to_char(to_number(web_start_time,'999999') - 240000,'9999')),'YYYY-MM-DDHH24MISS') + INTERVAL '1day'
+   WHEN to_number(web_start_time,'999999') >= 240100 AND to_number(web_start_time,'999999') < 241000
+    THEN to_timestamp(web_date||'000'|| TRIM(to_char(to_number(web_start_time,'999999') - 240000,'999')),'YYYY-MM-DDHH24MISS') + INTERVAL '1day' 
+   WHEN to_number(web_start_time,'999999') >= 240010 AND to_number(web_start_time,'999999') < 240100
+    THEN to_timestamp(web_date||'0000'||TRIM(to_char(to_number(web_start_time,'999999') - 240000,'99')),'YYYY-MM-DDHH24MISS') + INTERVAL '1day'
+   WHEN to_number(web_start_time,'999999') >= 240000 AND to_number(web_start_time,'999999') < 240010
+    THEN to_timestamp(web_date||'00000'|| TRIM(to_char(to_number(web_start_time,'999999') - 240000,'9')),'YYYY-MM-DDHH24MISS') + INTERVAL '1day'
+   ELSE to_timestamp(web_date||web_start_time,'YYYY-MM-DDHH24MISS') END AS web_start_datetime
   TO_NUMBER(pc_flag,'9'),
   TO_NUMBER(unknown1,'9'),
   TO_NUMBER(unknown2,'9'),
@@ -43,4 +49,4 @@ SELECT
   TO_NUMBER(web_time,'9999')
 FROM 
   original.t_weblog;
-   
+  
